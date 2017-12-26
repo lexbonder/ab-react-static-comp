92050f793633cc039f2724f92c2fb1be1b0a18ba
Alex Bonder <lexbonder@gmail.com>
2017-12-26 15:23:41 -0500

    Initial commit

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  // WHAT GOES HERE?
+  return (
+    <div className='Footer'>
+    © 2017
+    </div>
+  )
 }
 
 export default Footer;
--- src/Stories.js
+++ src/Stories.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import Story from './Story';
+import Author from './Author'
 import './Stories.css';
 
 const Stories = (props) => {
   return (
     <div className='Stories'>
-      <div className='  stories-header'>
+
+      <div className='stories-header'>
         <h3>{props.storiesTitle}</h3>
         <a href='#'>See more</a>
       </div>
+
       <div className='stories-grid'>
         {
           props.stories.map( (story, index) => {
-            console.log(story);
-            // INSTEAD OF returning the div below, return a Story component
-            // Be sure to pass down the correct props!
-            return (<div>{story.title}</div>)
+            return (<Story img={story.img} 
+                           title={story.title} 
+                           desc={story.desc} 
+                           authName={story.authName}
+                           authImg={story.authImg}
+                           estTime={story.estTime} />)
           })
         }
       </div>
